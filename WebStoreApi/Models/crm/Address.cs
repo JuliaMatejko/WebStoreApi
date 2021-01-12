94d55ec610ab1872a03d8504ac9809f6d476984a
Julia Matejko <matejkojulia@gmail.com>
2021-01-12 03:53:13 +0100

    Introduce simple crm

---
+++ WebStoreApi/Models/crm/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStoreApi.Models.crm
+{
+    public class Address
+    {
+        public ulong Id { get; set; }
+        public string Country { get; set; }
+        public string Locality { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+        public string BuildingNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+    }
+}
---
+++ WebStoreApi/Models/crm/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace WebStoreApi.Models.crm
+{
+    public class Client
+    {
+        public ulong Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public Address Address { get; set; }
+
+    }
+}
